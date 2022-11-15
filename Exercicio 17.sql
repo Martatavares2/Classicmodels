e159c6e6dbb370b77af605b1b060bb56258b9904
Martatavares2 <99660446+Martatavares2@users.noreply.github.com>
2022-11-15 14:28:34 +0000

    Add files via upload

---
+++ Exercicio 17.sql
@@ -0,0 +1,17 @@
+
+
+SELECT f.customerNumber, f.customerName, Total_billing, Total_paid, Total_billing-Total_paid as Debt
+FROM
+(SELECT o.customerNumber, customerName, sum(quantityOrdered*priceEach) AS Total_billing
+FROM classicmodels.orderdetails AS od
+INNER JOIN orders AS o ON od.orderNumber=o.orderNumber
+INNER JOIN customers AS c ON c.customerNumber=o.customerNumber
+GROUP BY customerNumber) AS f 
+
+INNER JOIN
+
+(SELECT customerNumber, SUM(amount) AS Total_paid
+FROM classicmodels.payments
+GROUP BY customerNumber) AS d ON f.customerNumber=d.customerNumber
+
+ORDER BY Debt DESC
